556a334994e28e71db0dc6f032e4d14a2d51f919
Fide <fidel2702@gmail.com>
2022-06-25 12:05:20 -0300

    estilos registered share gmail

--- src/components/views/registered/registered.jsx
+++ src/components/views/registered/registered.jsx
@@ -1,9 +1,33 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./registered.css";
 
 const Registered = () => {
   const { teamID } = useParams();
-  return <div>Te has registrado en el grupo {teamID} </div>;
+
+  return (
+    <div className="container-registered">
+      <div className="container-data">
+        <p>Te has registrado en el team: </p>
+        <p className="team-id">{teamID}</p>
+        <div className="container-share">
+          <p>Comparte el ID con tu equipo: </p>
+          <a
+            href={`mailto:@gmail.com?subject=Team ID&body= El ID de nuestro team es ${teamID}`}
+          >
+            <img
+              src="https://i.imgur.com/RL6XERO.png"
+              alt="gmail"
+              className="img-mail"
+            />
+          </a>
+        </div>
+      </div>
+      <Link to="/login" className="login-registered">
+        Login
+      </Link>
+    </div>
+  );
 };
 
 export default Registered;
